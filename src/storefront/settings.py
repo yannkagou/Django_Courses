7aa6468a35dddc6747d1a584f95321b10104c42f
Yannick Kagou <yannickkagou@gmail.com>
2024-09-20 23:35:09 +0100

    Feat: start the User Profile

--- src/storefront/settings.py
+++ src/storefront/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'playground',
     'store',
     'tags',
-    'store_custom'
+    'core'
 ]
 
 MIDDLEWARE = [
@@ -130,3 +130,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+AUTH_USER_MODEL = 'core.User'
